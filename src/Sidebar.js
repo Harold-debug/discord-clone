d49c95c149aeeb78363f8f152034906bc2565ced
Harold-debug <geumtchengh@gmail.com>
2021-11-13 18:56:14 +0100

    working on the side bar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Sidebar.css";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
+import SidebarChannel from "./SidebarChannel";
+import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import CallIcon from "@mui/icons-material/Call";
 
 function Sidebar() {
   return (
@@ -16,6 +21,26 @@ function Sidebar() {
             <ExpandMoreIcon />
             <h4>Text Channels</h4>
           </div>
+
+          <AddIcon className="sidebar_addChannel" />
+        </div>
+
+        <div className="sidebar_channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+      <div className="sidebar_voice">
+        <SignalCellularAltIcon className="sidebar_voiceIcon" fontSize="large" />
+        <div className="sidebar_voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
+        </div>
+        <div className="sidebar_voiceIcons">
+          <InfoOutlinedIcon />
+          <CallIcon />
         </div>
       </div>
     </div>
