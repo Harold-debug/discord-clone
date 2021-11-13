e755e7f472eaccbee2145e31b7290c7251186b40
Harold-debug <geumtchengh@gmail.com>
2021-11-13 20:34:53 +0100

    worked out the chat header

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -1,16 +1,32 @@
 import React from "react";
 import "./ChatHeader.css";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import EditLocationRoundedIcon from "@mui/icons-material/EditLocationRounded";
+import PeopleAltRoundedIcon from "@mui/icons-material/PeopleAltRounded";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import SendRoundedIcon from "@mui/icons-material/SendRounded";
+import HelpRoundedIcon from "@mui/icons-material/HelpRounded";
+
 function ChatHeader() {
   return (
     <div className="chatHeader">
-      <h2>i AM</h2>
-
       <div className="chatHeader_left">
         <h3>
-          <span className="chatHeder_hash">#</span>Youtube
+          <span className="chatHeader_hash">#</span>Youtube
         </h3>
       </div>
-      <div className="chatHeader_right"></div>
+      <div className="chatHeader_right">
+        <NotificationsIcon />
+        <EditLocationRoundedIcon />
+        <PeopleAltRoundedIcon />
+
+        <div className="chatHeader_search">
+          <input type="text" placeholder="Search" name="" id="" />
+          <SearchRoundedIcon />
+        </div>
+        <SendRoundedIcon />
+        <HelpRoundedIcon />
+      </div>
     </div>
   );
 }
