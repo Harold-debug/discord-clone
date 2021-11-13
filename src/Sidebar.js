f660fc252643f43d22bebffce531c1619324fe8f
Harold-debug <geumtchengh@gmail.com>
2021-11-13 02:07:48 +0100

    coding and styling sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Sidebar.css";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h2>I am the sidebar</h2>
-
       <div className="sidebar_top">
         <h3>Harold Geumtcheng</h3>
+        <ExpandMoreIcon />
+      </div>
+
+      <div className="sidebar_channels">
+        <div className="sidebar_channelsHeader">
+          <div className="sidebar_header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
+        </div>
       </div>
     </div>
   );
